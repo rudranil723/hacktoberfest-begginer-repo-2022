a5e0f2cef43d99415279eb3d6a04ae3130b27810
Mohammad Khan <95682232+Mohammadkhan18@users.noreply.github.com>
2022-10-24 21:40:36 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+num = int(input("enter a number"))
+factorial = 1
+if num < 0:
+  print("sorry factorial does not exist foe negative number")
+elif num == 0
+  print("the factorial of 0 is 1")
+else:
+  for i in range(1,num+1):
+    factorial = factorial*i
+   print("the factorial of",num,"is",factorial) 
