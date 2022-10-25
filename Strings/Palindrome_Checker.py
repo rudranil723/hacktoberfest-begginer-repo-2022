349557cfef385abfee876cc6b26b63af1fa6d6fc
avishagoyal08 <90690345+avishagoyal08@users.noreply.github.com>
2022-10-25 08:21:39 +0530

    Update Palindrome_Checker.py

--- Strings/Palindrome_Checker.py
+++ Strings/Palindrome_Checker.py
@@ -5,8 +5,11 @@ for i in range(t):            #looping for t test cases
     
     s=input("ENTER STRING: ") #taking input of the string
     
-    if s==s[::-1]:            #checking if the reverse of the string is equal to the original string
+    #if s==s[::-1]:            
                               #s[::-1] reverses the string in python
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:   #checking if the reverse of the string is equal to the original string
+   
         print("GIVEN STRING ",s," IS A VALID PALINDROME") #if True then print it is a palindrome
     else:
         print("GIVEN STRING ",s," IS NOT A VALID PALINDROME") #if False then print it is not a palindrome
